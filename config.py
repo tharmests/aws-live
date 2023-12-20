3851a0dc4a46fc3e427585bf859de6346ead46dd
tharmests <154388708+tharmests@users.noreply.github.com>
2023-12-20 08:38:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cceqkdnl4sow.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tharmes-employee"
+customregion = "us-east-1"
+
